912516cb358f76990b30f2f99ce97510d3f36216
Calvin Raveenthran <calvinraveenthran@gmail.com>
2015-08-11 22:18:09 -0400

    Edit & Add button addition
    With this changes you are able to :
    1) Add an item
    2) Delete an item
    3) Edit button Implemented

--- ToDoListTableViewController.m
+++ ToDoListTableViewController.m
@@ -23,22 +23,22 @@
     // Do any additional setup after loading the view.
     
     self.toDoItems = [[NSMutableArray alloc] init];
-    
-   // [self loadInitialData];
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    //self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // [self loadInitialData];
 }
 /*
-
-- (void)loadInitialData {
-    ToDoItem *item1 = [[ToDoItem alloc] init];
-    item1.itemName = @"Buy milk";
-    [self.toDoItems addObject:item1];
-    ToDoItem *item2 = [[ToDoItem alloc] init];
-    item2.itemName = @"Buy eggs";
-    [self.toDoItems addObject:item2];
-    ToDoItem *item3 = [[ToDoItem alloc] init];
-    item3.itemName = @"Read a book";
-    [self.toDoItems addObject:item3];
-}*/
+ - (void)loadInitialData {
+ ToDoItem *item1 = [[ToDoItem alloc] init];
+ item1.itemName = @"Buy milk";
+ [self.toDoItems addObject:item1];
+ ToDoItem *item2 = [[ToDoItem alloc] init];
+ item2.itemName = @"Buy eggs";
+ [self.toDoItems addObject:item2];
+ ToDoItem *item3 = [[ToDoItem alloc] init];
+ item3.itemName = @"Read a book";
+ [self.toDoItems addObject:item3];
+ }*/
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
@@ -66,6 +66,20 @@
     return cell;
 }
 
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        //remove the deleted object from your data source.
+        //If your data source is an NSMutableArray, do this
+        [self.toDoItems removeObjectAtIndex:indexPath.row];
+        [self.tableView reloadData]; // tell table to refresh now
+    }
+}
+
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
@@ -91,14 +105,15 @@
     
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
+/*
+ #pragma mark - Navigation
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
